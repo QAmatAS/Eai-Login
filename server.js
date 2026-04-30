457ce532ab0412d759125c483b73efb33d971419
Narendra Atha Abhinaya <71386909+QAmatAS@users.noreply.github.com>
2026-04-30 08:55:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const routeAuth = require('./routes/Routes-Data');
 const app = express();
 const port = process.env.PORT;
 // const MONGODB_URI = process.env.MONGODB_URI;
-const MongoDB_URI = 'mongodb+srv://Vercel-Admin-ToA:J3UV7LZSB1HYzhdg@toa.9likqlg.mongodb.net/?retryWrites=true&w=majority';
+const MONGODB_URI = 'mongodb+srv://Vercel-Admin-ToA:J3UV7LZSB1HYzhdg@toa.9likqlg.mongodb.net/?retryWrites=true&w=majority';
 
 // Middleware
 app.use(bodyParser.json());
